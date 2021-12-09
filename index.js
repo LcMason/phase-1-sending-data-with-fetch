1593dbaabe7954cbb87db44a2b3b1adbbcdbb6c9
Lionel Mason <capitalcookhouse@gmail.com>
2021-12-08 22:38:00 -0500

    commit tree ff369fb309721db210159eaa070f661a3b0af3a2

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+let name = 'Lionel'
+let email = 'Lionel@Lionel.com'
+
+const submitData = (name, email) => {
+
+    const configurationObject = {
+        methond: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: 'Lionel',
+            email: 'Lionel@Lionel.com'
+        })
+    };
+
+    fetch("http://localhost:3000/users", configurationObject)
+
+.then(function (response) {
+        return response.json();
+    })
+    .then(function (object){
+        document.body.innerHTML = object[ "id" ]
+    } )
+
+.catch(function (error){
+    document.body.innerHTML = error.message
+    } )
+}
+
+
+
+
